b735d0fd06daf63ceb7461b748de9a22369a806a
mapllr <maplr3656@gmail.com>
2025-12-26 04:49:17 -0500

    Fixed: if the garbage file not found , it will creat one

--- scripts/save.py
+++ scripts/save.py
@@ -1,12 +1,16 @@
 import datetime
+import os 
 
 
 class Save:
     def __init__(self):
-        self.save_path = "data/" 
+        self.save_path = "garbage/" 
         self.file_name = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
 
     def save_csv(self, raw_data):
+        if not os.path.exists(self.save_path): 
+            os.mkdir(self.save_path)
+
         with open(f"{self.save_path}{self.file_name}.csv", "w") as file:
             for sen in raw_data:
                 parts = sen.split()
